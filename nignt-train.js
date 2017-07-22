566babf00808baf141fdf9249f39f370f8c44f5d
qgp9 <pauljang9@gmail.com>
2017-07-22 08:17:28 +0300

    fix typos

--- nignt-train.js
+++ nignt-train.js
@@ -21,7 +21,7 @@ class Train {
   async run (trainname, goods) {
     for (wagon of this.trains[trainname]) {
       await wagon[trainname](goods)
-        .catch(e => { console.error(`Eorros of wagon ${wagon.name} in train ${tranname}.`); throw Error })
+        .catch(e => { console.error(`Erros of wagon ${wagon.name} in train ${tranname}.`); throw Error })
     }
   }
 }
