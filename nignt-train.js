4a9afad4dd0f6b1d993b0228707ef26aeac45009
qgp9 <pauljang9@gmail.com>
2017-07-22 08:09:33 +0300

    init

---
+++ nignt-train.js
@@ -0,0 +1,27 @@
+class Train {
+  constructor(trainNames) {
+    this.wagons = {}
+    this.trainNames = [
+      'register',
+      ...trainNames
+    ]
+    this.trains = {}
+    for (const name of this.trainNames) this.trains[name] = []
+  }
+
+  register (wagon) {
+    if (!wagon) throw Error ('Wagon is empty')
+    if (!wagon.register) throw Error('Wagon has no register method. ', wagon)
+    this.wagons(wagon.name) = this.trains.register.length
+    for (const name of this.trainNames) {
+      if (wagon[train]) this.trains[name].push(wagon)
+    }
+  }
+
+  await run (trainname, goods) {
+    for (wagon of this.pluginsImp[trainname]) {
+      await wagon[trainname](goods)
+        .catch(e => { console.error(`Eorros of wagon ${wagon.name} in train ${tranname}.`); throw Error })
+    }
+  }
+}
