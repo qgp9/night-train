be6e4dc2481313da0b35ee4820644d49eb3c4684
qgp9 <pauljang9@gmail.com>
2017-07-24 09:13:40 +0300

    add self inspection for wagon

--- nignt-train.js
+++ nignt-train.js
@@ -1,8 +1,20 @@
+'use strict'
+
+function ERROR (...args) {
+  return function (err) {
+    console.error(...args)
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+const REGISTER = 'register'
+
 class Train {
   constructor(trainNames) {
     this.wagons = {}
     this.trainNames = [
-      'register',
+      REGISTER,
       ...trainNames
     ]
     this.trains = {}
@@ -11,17 +23,49 @@ class Train {
 
   register (wagon) {
     if (!wagon) throw Error ('Wagon is empty')
-    if (!wagon.register) throw Error('Wagon has no register method. ', wagon)
-    this.wagons[wagon.name] = this.trains.register.length
+    if (!wagon[REGISTER]) throw Error('Wagon has no register method. ', wagon)
+    const index = this.trains[REGISTER].length
+    this.wagons[wagon.name] = index
+    this.trains.register.push([index, wagon])
     for (const name of this.trainNames) {
-      if (wagon[train]) this.trains[name].push(wagon)
+      if (name === REGISTER) continue
+      if (wagon[name]) this.trains[name].push([index, wagon])
     }
   }
 
   async run (trainname, goods) {
-    for (wagon of this.trains[trainname]) {
-      await wagon[trainname](goods)
-        .catch(e => { console.error(`Erros of wagon ${wagon.name} in train ${tranname}.`); throw Error })
+    for (const index in this.trains[trainname]) {
+      const wagonInfo = this.trains[trainname][index]
+      const registerIndex = wagonInfo[0]
+      const wagon = wagonInfo[1]
+      await wagon[trainname](goods, {
+        index,
+        registerIndex,
+        trains: this.trains,
+        wagons: this.wagos
+      })
+        .catch(ERROR(`Erros of wagon ${wagon.name} in train ${trainname}.`))
     }
   }
+
+  async runAsync (trainname, goods) {
+    const plist = []
+    for (const index in this.trains[trainname]) {
+      const wagonInfo = this.trains[trainname][index]
+      const registerIndex = wagonInfo[0]
+      const wagon = wagonInfo[1]
+      const promise = wagon[trainname](goods, {
+        index,
+        registerIndex,
+        trains: this.trains,
+        wagons: this.wagos
+      })
+        .catch(e => { console.error(`Erros of wagon ${wagon.name} in train ${trainname}.`); throw Error })
+      plist.push(promise)
+    }
+    await Promise.all(plist)
+  }
+
 }
+
+module.exports = Train
